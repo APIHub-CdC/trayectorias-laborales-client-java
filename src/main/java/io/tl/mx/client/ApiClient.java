4c3337bbf0ed0dd9f45187c1f95d5b75d4a21f7f
BryantCancino <bryantcancino@gmail.com>
2020-09-02 15:21:54 -0500

    Cambio metodo SSL

--- src/main/java/io/tl/mx/client/ApiClient.java
+++ src/main/java/io/tl/mx/client/ApiClient.java
@@ -369,34 +369,42 @@ public class ApiClient {
 			}
 		}
 	}
-	@SuppressWarnings("deprecation")
 	public static OkHttpClient getClientNoSSLVerification() {
 		try {
-			final TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
-				@Override
-				public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType)
-						throws CertificateException {
-				}
-				@Override
-				public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType)
-						throws CertificateException {
-				}
-				@Override
-				public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-					return new java.security.cert.X509Certificate[] {};
-				}
-			} };
+			final TrustManager[] trustAllCerts = new TrustManager[] {
+				new X509TrustManager() {
+					@Override
+					public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType)
+							throws CertificateException {}
+	
+					@Override
+					public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType)
+					throws CertificateException {}
+		
+					@Override
+					public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+						return new java.security.cert.X509Certificate[] {};
+					}
+				} 
+			};
+			
+			X509TrustManager trustManager = (X509TrustManager) trustAllCerts[0];
 			final SSLContext sslContext = SSLContext.getInstance("SSL");
 			sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
 			final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
-			Builder okHttpClient = new OkHttpClient().newBuilder().sslSocketFactory(sslSocketFactory)
-					.hostnameVerifier(new HostnameVerifier() {
+			
+			Builder okHttpClient = new OkHttpClient()
+					.newBuilder()
+					.sslSocketFactory(sslSocketFactory, trustManager)
+					.hostnameVerifier(new HostnameVerifier() {	
 						@Override
 						public boolean verify(String hostname, SSLSession session) {
 							return true;
 						}
 					});
+			
 			return okHttpClient.build();
+
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
